0690dd81147fcee79de7dfcda18fac6b09155752
Tepenox <anaselafya@gmail.com>
2020-04-11 21:19:51 +0200

    added export.createHelpRequest in model.js

--- model.js
+++ model.js
@@ -8,3 +8,8 @@ exports.createUser =function(formInput){
     var id = db.prepare('INSERT INTO users (first_name,last_name,age,email,password,description) VALUES (@first_name,@last_name,@age,@email,@password,@description)').run(form).lastInsertRowid;
     return id;
 }
+
+exports.createHelpRequest(helpRequest) {
+  var id = db.prepare("INSERT INTO recipe (owner_id, date, description) VALUES (@owner_id, datetime('now'), @description)").run(helpRequest).lastInsertRowid;
+    return id ;
+}
