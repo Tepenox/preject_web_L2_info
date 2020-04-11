17c5f96498548088a36e0fa4ad1d9fc77bf2bf56
Tepenox <anaselafya@gmail.com>
2020-04-11 21:28:42 +0200

    fixed createHelpRequest sysntax error in model.js

--- model.js
+++ model.js
@@ -5,11 +5,11 @@ const Sqlite = require('better-sqlite3');
 let db = new Sqlite('database.sqlite');
 
 exports.createUser =function(formInput){
-    var id = db.prepare('INSERT INTO users (first_name,last_name,age,email,password,description) VALUES (@first_name,@last_name,@age,@email,@password,@description)').run(form).lastInsertRowid;
+    var id = db.prepare('INSERT INTO users (first_name,last_name,age,email,password,description) VALUES (@first_name,@last_name,@age,@email,@password,@description)').run(formInput).lastInsertRowid;
     return id;
-}
+};
 
-exports.createHelpRequest(helpRequest) {
+exports.createHelpRequest= function (helpRequest) {
   var id = db.prepare("INSERT INTO recipe (owner_id, date, description) VALUES (@owner_id, datetime('now'), @description)").run(helpRequest).lastInsertRowid;
     return id ;
 }
